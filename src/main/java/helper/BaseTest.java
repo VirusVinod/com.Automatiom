be1b61415b662708c044d0f7ebee22d0fd9c0f32
vinod singh <vinodkumarsingh680@gmail.com>
2025-12-04 15:59:11 +0530

    first commit

---
+++ src/main/java/helper/BaseTest.java
@@ -0,0 +1,31 @@
+package helper;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class BaseTest {
+
+	WebDriver driver;
+
+	public void selectValueFromVisibleText(WebElement ele, String Text) {
+		Select s = new Select(ele);
+		s.selectByValue(Text);
+	}
+
+	public void clickOnElement(WebElement ele) {
+		try {
+			ele.click();
+		} catch (Exception e) {
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("arguments[0].click()", ele);
+		}
+	}
+
+	public void clearAndText(WebElement ele, String inputText) {
+		ele.clear();
+		ele.sendKeys(inputText);
+	}
+
+}
---
+++ src/test/java/com/Automatiom/com/Automatiom/AppTest.java
@@ -0,0 +1,19 @@
+package com.Automatiom.com.Automatiom;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
